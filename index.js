44ca1d279e5936db7a06dfc74b188828e6a4cd53
Gerbert Pesquera <gerbert.pesquera@gmail.com>
2022-02-01 20:32:11 -0500

    commit tree aa9b0ca5d263f3ce8ac90d177cd5b37c9fea916e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+const currentUser = 'Revere Pesquera';
+// Defining current user, 
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// Defining "welcomeMessage" containing greeting and interpolation from "currentUser"
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// Defining "excitedWelcomeMessage" containing "welcomeMessage" then being capitalized using ".toUpperCase()"
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+// Defining "shortGreeting" that returns only first letter of "currentUser" using ".slice(0,1)"
